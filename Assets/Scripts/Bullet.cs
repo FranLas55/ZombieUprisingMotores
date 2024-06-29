e1649b011772e6fc9931ca564fa0ddeb7b941354
TobiRV <tobirodriguezvieyra@gmail.com>
2024-06-28 21:03:17 -0300

    agregue armas
    scripts con herencia

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        private int _damage = 0;
+
+        private Rigidbody _rb;
+
+        private float _lifeTime;
+
+        //SetDamage y SetDir en uno solo
+        public void InitializeBullet(int damage, float lifeTime)
+        {
+            _damage = damage;
+            _lifeTime = lifeTime;
+        }
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            _rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+
+        public void Shot(Vector3 dir)
+        {
+            //Destroy(gameObject, _lifeTime);
+            transform.SetParent(null);
+            _rb.constraints = RigidbodyConstraints.None;
+
+            _rb.velocity = dir;
+        }
+
+
+        /*private void OnCollisionEnter(Collision collision)
+        {
+            //hacer daño al enemigo
+            if (collision.gameObject.TryGetComponent<PlayerMovement>(out PlayerMovement player))
+            {
+                player.TakeDamage(_damage);
+                print($"Le pegue al player");
+            }
+
+            Destroy(gameObject);
+        }*/
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
